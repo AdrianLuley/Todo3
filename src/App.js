35ad0b4ccf776a6c94b023b22a2a4dff888f6388
Adrian Luley <amluley@comcast.net>
2018-08-13 15:55:31 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
-import todoList from './todos.json';
+import {Route ,BrowserRouter, Switch} from 'react-router-dom';
 import './index.css';
 import ToDoList from './ToDoList';
+import defaultTodos from './todos.json';
+
 
 
 
 class App extends Component {
+  state = {
+		todos: defaultTodos
+   }
+   handleClick = (id) => () => {
+	   const todos = this.state.todos
+		for(let i=0; i < todos.length; i++) {
+		if (todos[i].id === id) {
+			todos[i].completed = !todos[i].completed 
+		} 
+		}
+   }
+   addTodo = (e) => {
+    const { todos } = this.state;
+    const title = document.getElementById("text-input").value;
+    if(e.key === "Enter") {
+      this.setState({todos: [...todos, {
+        "userId": 1,
+        "id": todos[todos.length -1].id + 1,
+        "title": title,
+        "completed": false
+      }]});
+      document.getElementById("text-input").value = ""
+    }
+  }
+  
+  deleteItem = id => () => {
+    const { todos } = this.state;
+    
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    })
+  }
+  
+  deleteAllItems = () => {
+    const { todos } = this.state;
+    this.setState({todos: todos.filter(todo => !todo.completed)})
+  }
   render() {
+    const todos = this.state.todos;
     return (
       <div className="App">
-      
-      <ToDoList/>
+      		<BrowserRouter>
+					<Switch>
+          <Route exact path='/' render={() => <ToDoList todos={todos} handleClick={this.handleClick} deleteItem={this.deleteItem}/>}/>
+          <Route path='/active' render={() => <ToDoList todos={todos.filter(todo => !todo.completed)} taskCompleted={this.handleClick} deleteItem={this.deleteItem}/>}/>
+          <Route path='/completed' render={() => <ToDoList todos={todos.filter(todo => todo.completed)} handleClick={this.handleClick} deleteItem={this.deleteItem}/>}/>
+        </Switch>
+				</BrowserRouter>
        
         </div>
     );
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,45 +1,10 @@
 import React from "react";
+
 import ToDoItem from './ToDoItem';
-import todos from './todos.json';
+
 
 class ToDoList extends React.Component {
-   state = {
-		todos: todos
-   }
-   handleClick = (id) => () => {
-	   const todos = this.state.todos
-		for(let i=0; i < todos.length; i++) {
-		if (todos[i].id === id) {
-			todos[i].completed = !todos[i].completed 
-		} 
-		}
-   }
-   addTodo = (e) => {
-    const { todos } = this.state;
-    const title = document.getElementById("text-input").value;
-    if(e.key === "Enter") {
-      this.setState({todos: [...todos, {
-        "userId": 1,
-        "id": todos[todos.length -1].id + 1,
-        "title": title,
-        "completed": false
-      }]});
-      document.getElementById("text-input").value = ""
-    }
-  }
-  
-  deleteItem = id => () => {
-    const { todos } = this.state;
-    
-    this.setState({
-      todos: todos.filter(todo => todo.id !== id)
-    })
-  }
-  
-  deleteAllItems = () => {
-    const { todos } = this.state;
-    this.setState({todos: todos.filter(todo => !todo.completed)})
-  }
+
 		render() {
 		
 			return(
@@ -47,20 +12,38 @@ class ToDoList extends React.Component {
                		<section className="todoapp">
 						<header className="header">
 							<h1>todos</h1>
-							<input id="text-input" className="new-todo" placeholder="What needs to be done?" autoFocus onKeyPress={this.addTodo}/>
+							<input id="text-input" className="new-todo" placeholder="What needs to be done?" autoFocus onKeyPress={this.props.addTodo}/>
 						</header>
 						<section className="main">
 							<ul className="todo-list">
-					{this.state.todos.map( todo => <ToDoItem key = {todo.id}  handleClick = {this.handleClick(todo.id)} title = {todo.title} 
+					{this.props.todos.map( todo => <ToDoItem key = {todo.id}  handleClick = {this.props.handleClick(todo.id)} title = {todo.title} 
 					     completed = {todo.completed} deleteItem={this.props.deleteItem}/> )}
 					</ul>
 					</section>
+			
 					<footer className="footer">
 
 			
-					<span className="todo-count"><strong>{this.state.todos.length}</strong> item(s) left</span>
-		
-					<button className="clear-completed" onClick={this.deleteAllItems}>Clear Completed</button>
+					<span className="todo-count"><strong>{this.props.todos.length}</strong> item(s) left</span>
+					<ul className="filters">
+						<li>
+							<a href="#/">
+							All
+							</a>
+						</li>
+						<li>
+							<a href="#/active">
+							Active
+							</a>
+						</li>
+						<li>
+							<a href="#/completed">
+							Completed
+							</a>
+						</li>
+						</ul>
+						
+					<button className="clear-completed" onClick={this.props.deleteAllItems}>Clear Completed</button>
 					</footer>
 					</section>
 				</div>
